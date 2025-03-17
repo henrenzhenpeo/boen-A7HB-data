0c4f687c6d86bb192d007c7fbc83e07b50ad3c77
henrenzhenpeo <115695966+henrenzhenpeo@users.noreply.github.com>
2025-03-17 14:09:18 +0800

    Add files via upload

---
+++ boen-HB-data/src/main/java/org/example/boenhbdata/BoenHbDataApplication.java
@@ -0,0 +1,13 @@
+package org.example.boenhbdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoenHbDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoenHbDataApplication.class, args);
+    }
+
+}
---
+++ boen-HB-data/src/test/java/org/example/boenhbdata/BoenHbDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boenhbdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoenHbDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
